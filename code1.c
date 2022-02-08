0089bbed4cff2448c62fc03e1899c93fbb48e0fa
latikadekate123 <99274026+latikadekate123@users.noreply.github.com>
2022-02-08 21:51:27 +0530

    Created MyCaptain Project1 Code2
    C++ program to find size of datatypes

---
+++ code1.c
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char x;
+    int y;
+    float z;
+    double w;
+    cout<<"\n Size of char:"<<sizeof(x)<<" byte";
+    cout<<"\n Size of int:"<<sizeof(y)<<" bytes";
+    cout<<"\n Size of float:"<<sizeof(z)<<" bytes";
+    cout<<"\n Size of double:"<<sizeof(w)<<" bytes";
+    return 0;
+}
